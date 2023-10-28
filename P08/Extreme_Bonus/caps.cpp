93207b7f60707f4a0980d6a065c4af4a9f265243
Shaun Jacob <ssj9132@mavs.uta.edu>
2023-10-28 15:49:44 -0500

    Extreme_Bonus

---
+++ P08/Extreme_Bonus/caps.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+
+int main(int args, char* argv[])
+{
+	std::vector<std::string> caps;
+	std::vector<std::string>* no_caps = new std::vector<std::string>;
+	for(int i = 1; i<args; i++)
+	{
+		std::string word;
+		word = argv[i];
+		
+		if(argv != NULL && isupper(word[0]))
+		{
+			caps.push_back(word);
+			sort(caps.begin(),caps.end());
+
+		}
+		else
+		{
+			no_caps->push_back(word);
+		}
+	}
+	
+	std::cout<<"Capitalized: "<<std::endl;
+	for(auto j: caps)
+	{
+		std::cout<<j<<std::endl;
+	}
+	std::cout<<"Lower Case: "<<std::endl;
+	for(auto k: *no_caps)
+	{
+		std::cout<<k<<std::endl;
+	}
+}
